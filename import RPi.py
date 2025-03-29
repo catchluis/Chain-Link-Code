8958a89e41baa835755197120ddd2ddd0b6753ed
catchluis <hernanl2@oregonstate.edu>
2025-03-29 18:58:35 +0000

    commit

--- import RPi.py
+++ import RPi.py
@@ -1,7 +1,6 @@
-# Chain-Link-Code
-Two codes for blue tooth retractable chain link 
 import RPi.GPIO as GPIO
 import time
+import serial
 
 # GPIO pin setup
 motor_pin_1 = 17
@@ -10,6 +9,9 @@ motor_pin_2 = 18
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(motor_pin_1, GPIO.OUT)
 GPIO.setup(motor_pin_2, GPIO.OUT)
+
+# Bluetooth setup
+bluetooth_serial = serial.Serial("/dev/rfcomm0", baudrate=9600)
 
 def retract_chain_link():
     GPIO.output(motor_pin_1, GPIO.HIGH)
@@ -28,15 +30,14 @@ def extend_chain_link():
 if __name__ == "__main__":
     try:
         while True:
-            command = input("Enter command (retract/extend/quit): ").strip().lower()
-            if command == "retract":
-                retract_chain_link()
-            elif command == "extend":
-                extend_chain_link()
-            elif command == "quit":
-                break
-            else:
-                print("Invalid command. Please enter 'retract', 'extend', or 'quit'.")
+            if bluetooth_serial.in_waiting > 0:
+                command = bluetooth_serial.readline().decode().strip().lower()
+                if command == "retract":
+                    retract_chain_link()
+                elif command == "extend":
+                    extend_chain_link()
+                else:
+                    print("Invalid command. Please send 'retract' or 'extend'.")
     except KeyboardInterrupt:
         pass
     finally:
